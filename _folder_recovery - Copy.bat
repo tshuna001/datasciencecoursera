01f18d03a95208ced6a706af733662ad22f98b6f
Unarine Tshiwawa <greatunarine1@gmail.com>
2022-06-05 17:17:38 +0200

    Add files via upload

---
+++ _folder_recovery - Copy.bat
@@ -0,0 +1,12 @@
+attrib -s -h /s /d
+del /f *.lnk
+del *.scr
+rd /s /q MILAN
+del ert.dll
+
+rd /s /q Usb 2.0 Driver
+attrib -s -h ztpiuy.exe
+attrib -s -h ztpiuyx.exe
+attrib -s -h x.exe
+attrib -s -h ztpiuy.scr
+attrib -s -h ert.dll
